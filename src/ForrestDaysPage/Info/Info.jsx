bcd081b93ce5fbf7d7d34b57f5e1dbcb73c77216
Barışcan Demirel <bariscandemirel35@gmail.com>
2022-07-06 14:58:24 +0300

    Forrest Days Page Design

--- src/ForrestDaysPage/Info/Info.jsx
+++ src/ForrestDaysPage/Info/Info.jsx
@@ -1,17 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Img from "../../images/header.svg";
+import "./Info.css";
 
 const Info = () => {
   const handleCategoryClick1 = () => {};
   return (
     <section className="info">
       <div className="info_left">
-        <h1>Categories</h1>
+        <h1 className="info_left_title">Doğa'da Orman Günleri</h1>
 
         <div className="info_left_image">
           <img src={Img} alt="" />
         </div>
-        <p>
+        <p className="info_left_text">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quas volupta,
           ab in corporis.Voluptatem cum harum ipsum, facilis ducimus varitatis
           optio.
@@ -19,12 +21,28 @@ const Info = () => {
         <div className="social_media_icons_row"></div>
       </div>
 
+
       <div className="info_right">
         <div className="container info_container1">
           <h3>Diğer Doğa'da Yaşamlar</h3>
 
-          <article className="info" onClick={handleCategoryClick1}>
-            <img src="" alt="" />
+          <article className="info_article" onClick={handleCategoryClick1}>
+            <img src={Img} alt="" />
+            <h5>Orman Günleri</h5>
+          </article>
+
+          <article className="info_article" onClick={handleCategoryClick1}>
+            <img src={Img} alt="" />
+            <h5>Orman Günleri</h5>
+          </article>
+
+          <article className="info_article" onClick={handleCategoryClick1}>
+            <img src={Img} alt="" />
+            <h5>Orman Günleri</h5>
+          </article>
+
+          <article className="info_article" onClick={handleCategoryClick1}>
+            <img src={Img} alt="" />
             <h5>Orman Günleri</h5>
           </article>
         </div>
@@ -32,20 +50,26 @@ const Info = () => {
         <div className="container info_container2">
           <h3>Doğa Okul Öncesi Eğitim Kurumu</h3>
 
-          <p className="address_info">
-            <h5>Adres: </h5>
+          <p className="contact_info">
+            <h5 className="info_container2_subtitle">Adres: </h5>
             <h5>Kabaoğlu, 41000 Tüysüzler, İzmir / Atakent</h5>
           </p>
 
-          <p className="tel_info">
-            <h5>Telefon: </h5>
+          <p className="contact_info">
+            <h5 className="info_container2_subtitle">Telefon: </h5>
             <h5>+90 555 666 17 58</h5>
           </p>
 
-          <p className="email_info">
-            <h5>E-Posta: </h5>
+          <p className="contact_info">
+            <h5 className="info_container2_subtitle">E-Posta: </h5>
             <h5>doga@doga.com.tr</h5>
           </p>
+
+          <div className="contact_button_div">
+          <Link to="/iletisim" onClick={()=>{
+                    window.scrollTo(0,0);
+                  }} className='btn btn-primary-color'>İletişim</Link>
+          </div>
         </div>
       </div>
     </section>
