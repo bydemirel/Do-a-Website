085004ecf9a528999db150597522b7db1b62fa30
Barışcan Demirel <bariscandemirel35@gmail.com>
2022-08-29 10:06:22 +0300

    Development Areas Page

--- src/DevelopmentAreasPage/DevelopmentAreas/DevelopmentAreas.jsx
+++ src/DevelopmentAreasPage/DevelopmentAreas/DevelopmentAreas.jsx
@@ -1,56 +1,170 @@
-import React from 'react'
+import React from "react";
 import "../../AboutUsPage/Achievements/Achievements";
 import AchievementsImage from "../../images/about achievements.svg";
 
 const DevelopmentAreas = () => {
   return (
     <section className="about_achievements">
-    <div className="container about_achievements-container">
-      <div className="about_achievements-left">
-        <img src={AchievementsImage} alt="" />
-        <p>
-          <br /><br />
-          Almanya’da uygulanan orman ve doğa Pedagojisini, Türk Milli Eğitim
-          Müfredatına uygun bir şekilde okul öncesi ve ilkokullarda görev
-          yapan eğitimci ve öğretmenlere bireysel eğitim sağlamayı, özel
-          kurumlara kurum içi eğitimlerle orman okulunda yapılacak yıllık
-          planların hazırlanmasında, projelerin yürütülmesinde, kıyafet,
-          ekipman, alan seçimi, güvenlik önlemleri alanında bilgilendirerek
-          destek olmayı amaçlıyoruz.
+      <div className="container about_achievements-container">
+        <div className="about_achievements-left">
+          <img src={AchievementsImage} alt="" />
           <br />
           <br />
-          Diğer bir hedefimiz de devlet anaokulu ve ilkokullarında görev yapan
-          öğretmenlere orman günü planlamalarında etkinlik hazırlama, etkinlik
-          ve proje yürütmede destek olmak.
-        </p>
-      </div>
+          <h4 className="subtitle">BİLİŞSEL GELİŞİM ALANI: GÖRSEL</h4>
+          <p>
+            -Yol güzergahını ve çizgileri takip etme.
+            <br />
+            -Varlıkların alandaki durumunu algılama.
+            <br />
+            -Gözlemlenen yapıları, sıralamaları kaydetmek ve tekrar
+            kullanabilme.
+            <br />
+            -Dikkat süresi ne kadar.
+            <br />
+            -Cisim ve şekilleri basitten karmaşığa düzenleme.
+          </p>
+          <h4 className="subtitle">GELİŞİM ALANI: DUYUŞSAL</h4>
+          <p>
+            -Sesin nerden geldiğini tanıyabilme.
+            <br />
+            -Duyduğu sesleri sırasıyla tekrar etme.
+            <br />
+            -Bir sesi veya çıtırtıyı ayırt edebilme. (flitreleme)
+            <br />
+            -Bir hikayenin veya sözlü ifadelerin önemli noktalarını hatırlama.
+            <br />
+            -Bir ritmi veya alkış sırasını kısa bir süre için hatırlayabilme.
+            <br />
+            -Tekerlemeler veya sözcüklerdeki heceleri ayırtabilme. (phonetik)
+          </p>
 
-      <div className="about_achievements-right">
-        <h1>Neden Doğa</h1>
-        <p>
-          Özellikle son birkaç yıldır, çocukların daha fazla doğanın içinde
-          vakit geçirmelerini sağlayacak projeler önem kazanmaya başladı. Doğa
-          eğitiminin yanı sıra orman okulları alanında da yeni girişimler
-          görülmekte. Anne babalar ve eğitmeler çocukların doğayla temas
-          kurmasının ve hayatı doğanın içinde deneyimlemesinin önemini fark
-          etti.
+          <img src={AchievementsImage} alt="" />
           <br />
           <br />
-          Biz de doğaya yabancılaşan hatta doğanın farkında olmayan çocukların
-          öğretmen ya da anne babaları olarak bu kopukluğu gidermek ve bu bağı
-          yeniden inşa etmek istiyoruz. Misyonumuz, çeşitli etkinlikler
-          sayesinde çocuklarımızın doğanın içinde kendilerinden başka
-          canlıları gözlemlemesini, onları koruyup kollamasını öğrenmeleri,
-          sorumluluk almaları ve doğanın öğreticiliğini fark etmelerini
-          sağlamak. 3-5 yaş grubu çocukların öğretmenlerine, doğada tek başına
-          ve hafta sonu geçiren ailelere etkinliklerimizle destek olmayı
-          amaçlıyoruz.
-        </p>
-        <img src={AchievementsImage} alt="" />
-      </div>
-    </div>
-  </section>
-  )
-}
+          <h4 className="subtitle">GELİŞİM ALANI: SAYISAL / MATEMATİK</h4>
+          <p>
+            -Belirli çoklukları tahmin etme.
+            <br />
+            -Çoklukları gruplandırma.
+            <br />
+            -Sayıları çokluklara göre sınıflandırma.
+            <br />
+            -Belirli çoklukları saymadan kavrayabilme.
+            <br />
+            -Geometrik şekilleri tanıma.
+            <br />
+            -İleri geri saymaların düzeyi.
+            <br />
+            -Çoklukları karşılaştırma.
+            <br />
+            -Belli eşyaları uygun şekillerde gruplandırma.
+            <br />
+            -Büyüklüklerine göre gruplandırma.
+            <br />
+            -Renklerine şekil ve büyüklüklere göre gruplandırma.
+            <br />
+            -Karışık verilen varlıkları uygun gruplara sınıflandırabilme.
+          </p>
+          <h4 className="subtitle">GELİŞİM ALANI: KONSANTRASYON</h4>
+          <p>
+            -Dikkatini uzun süre tutabilme, kaybetmeme.
+            <br />
+            -Oyunda kalabilme süresi.
+            <br />
+            -Yeniden başlayabilme.
+            <br />
+            -Odaklanabilme, ilgi süresi.
+          </p>
+          <img src={AchievementsImage} alt="" />
+          <br />
+          <br />
+          <h4 className="subtitle">GELİŞİM ALANI: SOSYAL</h4>
+          <p>
+            -Diğer çocuklarla iletişime geçme.
+            <br />
+            -İş birliği içinde çalışma.
+            <br />
+            -Birden fazla oyun arkadaşı olması.
+            <br />
+            -Bireysel veya grup bilinciyle oynama.
+            <br />
+            -Lider kişilik.
+          </p>
+        </div>
 
-export default DevelopmentAreas
+        <div className="about_achievements-right">
+          <h2>
+            ORMAN VE DOĞA PEDAGOJİSİNDE GÖZLEMLENMESİ GEREKEN GELİŞİM ALANLARI
+          </h2>
+          <h4 className="subtitle">PSİKOMOTOR GELİŞİM ALANI: BEDEN VÜCUT</h4>
+          <p>
+            -El göz koordinasyonunun uyumu gözlemleme -Hareket eden varlıkları
+            gözleriyle takip edebilme.
+            <br />
+            -Vücudun merkezini işaretleyebilme.
+            <br />
+            -Nesnelere karşı gücünü uyumlu kullanabilme -Hareket ederken
+            koordinasyonu sağlama.
+            <br />
+            -Vücut dengesini sağlayabilme. (kaba motor)
+            <br />
+            -Dil ve dudak motoru -El parmak ile becerileri yapabilme (ince
+            motor.)
+          </p>
+          <img src={AchievementsImage} alt="" />
+          <br />
+          <br />
+          <h4 className="subtitle">GELİŞİM ALANI: DİL</h4>
+          <p>
+            -Kelimeleri kelime gruplarına göre düzenleme.
+            <br />
+            -Sözlü direktifleri anlama ve uygulayabilme.
+            <br />
+            -Yaşına uygun kelime dağarcığına sahip olma.
+            <br />
+            -Kurallı cümleler kurma.
+            <br />
+            -Kelime çeşitlerini anlamına uygun kullanabilme.
+            <br />
+            -Konuşmasını hareketlerle bütünleştirebilme.
+            <br />
+            -Bütün sesleri tanıma ve kullanma.
+            <br />
+            -Yer edatlarını doğru kullanma, altında, üstünde, içinde.
+          </p>
+          <img src={AchievementsImage} alt="" />
+          <br />
+          <br />
+          <h4 className="subtitle">GELİŞİM ALANI: ÇALIŞMA DÜZENİ</h4>
+          <p>
+            -Tek başına basit problemleri çözme.
+            <br />
+            -Karmaşık problemleri çözme.
+            <br />
+            -Sorunları çözerken sesli düşünme.
+            <br />
+            -Görevini ertesi gün de devam etme isteği.
+            <br />
+            -Görev sıralamasını gözlenebilme.
+            <br />
+            -Planlarını ifade etme.
+          </p>
+          <h4 className="subtitle">GELİŞİM ALANI: DUYGUSAL</h4>
+          <p>
+            -Diğer çocuklarla grup oluşturma.
+            <br />
+            -Arkadaşlarına üzüldüğünde destek yardım sunma; Kendini diğer
+            çocukların yerine koyabilme. (empati)
+            <br />
+            -Diğer çocukların isteklerini dikkate alma.
+            <br />
+            -Diğer çocuklarla kavgaya neden oluşturma.
+          </p>
+        </div>
+
+      </div>
+    </section>
+  );
+};
+
+export default DevelopmentAreas;
